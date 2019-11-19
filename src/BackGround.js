c08303d4ef96f66bc0df93d92aba6f2efedb955a
Takuro Mano <takuroh.mano@nts.ricoh.co.jp>
2019-11-20 08:47:55 +0900

    no message

--- src/BackGround.js
+++ src/BackGround.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import Box from '@material-ui/core/Box';
 import LogoImage from './resource/logo.png'
-import { ResponsiveContainer, ComposedChart, Line, Bar, Area, Scatter, XAxis,
-  YAxis, ReferenceLine, ReferenceDot, Tooltip, Legend, CartesianGrid, Brush,
-  LineChart } from 'recharts';
-import EnhancedTable from './table';
 
+import EnhancedTable from './table';
+import MyComposedChart from './myComposedChart';
 document.body.style.backgroundColor = "#FFd711";
 
 export default class BackGround extends Component {
@@ -27,42 +25,44 @@ export default class BackGround extends Component {
             display: "flex"
           },
           left:{
-            // background: "#fff",
+            // background: "#0ff",
             marginTop: "1%",
             marginLeft: "1%",
             paddingTop: "3px",
-            width: "18%",
+            width: "180px",
             height: "auto",
             minWidth: "180px",
             borderRadius: "10px",
           },
-          right: {
-            // background: "#3b5118",
-            // display: "block",
-            marginLeft: "1%",
-            marginTop: "1%",
-            width: "100%",
-            height: "auto",
-            // padding: "2px 10px",
-            borderRadius: "10px",
-            // cursor: "pointer",
-            // float: "left",
-            // lineHeight: "20px"
-          },
           table_box: {
             background: "#fff",
-            marginTop: "1%",
-            marginLeft: "1%",
-            width: "98%",
+            marginTop: "0%",
+            marginLeft: "0%",
+            width: "180px",
             height: 150,
+            minWidth: "180px",
             borderRadius: "10px",
+            position: "fixed",
+            // position: '-webkit-sticky',
+            // position: 'sticky',
+            // top: '0',
           },
           logo_box: {
             marginTop: "400px",
-            marginLeft: "10%",
+            marginLeft: "1%",
             marginBottom: "10%",
             width: "100%",
             height: 50,
+            position: "fixed",
+          },
+          right: {
+            // background: "#3b5118",
+            display: "block",
+            marginLeft: "1%",
+            marginTop: "1%",
+            width: "100%",
+            minWidth: "1080px",
+            borderRadius: "10px",
           },
           title_box: {
             background: "#fff",
@@ -103,11 +103,10 @@ export default class BackGround extends Component {
             marginLeft: "0%",
             paddingTop: "1%",
             width: "99%",
-            height: "700px",
             borderRadius: "10px",
           },
           answer_element: {
-            marginTop: "0.5%",
+            marginTop: "2%",
             marginLeft: "1%",
             width: "99%",
             height: "50px",
@@ -115,6 +114,7 @@ export default class BackGround extends Component {
             fontWeight: "bold",
             color: "#FFa711",
             borderBottom: "medium dotted",
+            float: "left",
           },
           composed_chart: {
 
@@ -124,14 +124,7 @@ export default class BackGround extends Component {
       }
     render() {
         const styles = this.styles();
-        const dataGraph = [
-          {month: '1月', '売上': 800, '総売上': 1400},
-          {month: '2月', '売上': 967, '総売上': 1506},
-          {month: '3月', '売上': 1098, '総売上': 989},
-          {month: '4月', '売上': 1200, '総売上': 1228},
-          {month: '5月', '売上': 1108, '総売上': 1100},
-          {month: '6月', '売上': 680, '総売上': 1700}
-      ];
+
       return (
               <Box style={styles.container}>
                 <Box style={styles.left}>
@@ -153,42 +146,22 @@ export default class BackGround extends Component {
                     <Box style={{fontSize: 16, color:"#888888"}}>サンプル用アンケート</Box>
                   </Box>
                   <Box style={styles.answer_box}>
-                    <Box style={styles.answer_element}>
-                      Q1 : hogehoge
-                    </Box>
-                    <ComposedChart //グラフ全体のサイズや位置、データを指定。場合によってmarginで上下左右の位置を指定する必要あり。
-                      width={400}  //グラフ全体の幅を指定
-                      height={400}  //グラフ全体の高さを指定
-                      data={dataGraph} //ここにArray型のデータを指定
-                      margin={{ top: 20, right: 60, bottom: 0, left: 0 }}  //marginを指定
-                      compact={false}
-                      style={{float: "left"}}
-                    >
-                      <XAxis
-                        dataKey="month"  //Array型のデータの、X軸に表示したい値のキーを指定
-                      />
-                      <YAxis />
-                      <Tooltip /> //hoverした時に各パラメーターの詳細を見れるように設定
-                      <Legend />  // 凡例を表示(図の【売上】【総売上】)
-                      <CartesianGrid //グラフのグリッドを指定
-                        stroke="#f5f5f5" //グリッド線の色を指定
-                      />
-                      <Area //面積を表すグラフ
-                        type="monotone"  //グラフが曲線を描くように指定。default値は折れ線グラフ
-                        dataKey="総売上" //Array型のデータの、Y軸に表示したい値のキーを指定
-                        stroke="#00aced" ////グラフの線の色を指定
-                        fillOpacity={1}  ////グラフの中身の薄さを指定
-                        fill="rgba(0, 172, 237, 0.2)"  //グラフの色を指定
-                      />
-                      <Bar //棒グラフ
-                        dataKey="売上"　//Array型のデータの、Y軸に表示したい値のキーを指定
-                        barSize={20}  //棒の太さを指定
-                        stroke="rgba(34, 80, 162, 0.2)" ////レーダーの線の色を指定 
-                        fillOpacity={1}  //レーダーの中身の色の薄さを指定
-                        fill="#2250A2" ////レーダーの中身の色を指定
-                      />
-                    </ComposedChart>
+                    <Box style={styles.answer_element}>Q1 : 1111111111111111111111111111</Box>
+                    <MyComposedChart></MyComposedChart>
                     <EnhancedTable></EnhancedTable>
+                    <Box style={{clear:'both'}}></Box>
+                  </Box>
+                  <Box style={styles.answer_box}>
+                    <Box style={styles.answer_element}>Q2 : 2222222222222222222222222222</Box>
+                    <MyComposedChart></MyComposedChart>
+                    <EnhancedTable></EnhancedTable>
+                    <Box style={{clear:'both'}}></Box>
+                  </Box>
+                  <Box style={styles.answer_box}>
+                    <Box style={styles.answer_element}>Q3 : 3333333333333333333333333333</Box>
+                    <MyComposedChart></MyComposedChart>
+                    <EnhancedTable></EnhancedTable>
+                    <Box style={{clear:'both'}}></Box>
                   </Box>
                 </Box>
               </Box>
